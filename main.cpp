cf3f8acc8e53b6342d69b3f2e9081e3f76b3fe63
akshay siripuram <akshaychinna86@gmail.com>
2026-09-22 17:55:09 +0530

    Updated program with search feature

--- main.cpp
+++ main.cpp
@@ -12,33 +12,58 @@ int main()
     vector<datta> dta;
     datta obj;
     char user;
+    char customer;
     while (true)
     {
-        cout << "please enter a name: ";
-        cin >> obj.name;
-        cout << "please enter a age: ";
-        cin >> obj.age;
-        cout << "please enter a country: ";
-        cin >> obj.country;
-        dta.push_back(obj);
+        cout<< "-------------------------------\n";
+        cout <<"|   option 1 for data entry:   |\n";
+        cout <<"|   option 2 to display data:  |\n";
+        cout <<"|   option 3 to exit:          |\n";
+        cout<< "-------------------------------\n";
+        cin >> customer;
+        if (customer == '1')
+        {
+            while (true)
+            {
+                cout << "please enter a name: ";
+                cin >> obj.name;
+                cout << "please enter a age: ";
+                cin >> obj.age;
+                cout << "please enter a country: ";
+                cin >> obj.country;
+                dta.push_back(obj);
+                cout << "Stored data number " << dta.size() << "\n";
 
-        cout<<"enter c to continue enter y to exit: ";
-        cin>>user;
-        if (user == 'c'){
-            cout<<"lets continue \n";
-        }else{
+                cout << "enter c to continue enter y to exit: ";
+                cin >> user;
+                if (user == 'c')
+                {
+                    cout << "lets continue \n";
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        else if (customer == '2')
+        {
+            cout << "\n";
+            cout << "stored data:  \n";
+            for (const datta &stu : dta)
+            {
+                cout << "name: " << stu.name << " \n"
+                     << "age: " << stu.age << " \n"
+                     << "country: " << stu.country << " \n";
+
+                cout << " \n";
+            }
+        }
+        else if (customer == '3')
+        {
+            cout << "Thank you bye ";
             break;
         }
     }
-    cout<<"\n";
-    cout<<"stored data:  \n";
-    for(const datta& stu : dta){
-        cout<<"name: "<<stu.name<<" \n"
-            <<"age: "<<stu.age<<" \n"
-            <<"country: "<<stu.country<<" \n";
-
-            cout<<" \n";
-    }
-
     return 0;
 }
